bc76c3d1c2fcf7126eced0151a9883ce9bb6ba2d
wxj_2021 <wangxinjian_01@163.com>
2021-09-24 12:18:53 +0800

    基于nacos的服务提供者

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @auther zzyy
+ * @create 2020-02-19 16:06
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping(value = "/payment/nacos/{id}")
+    public String getPayment(@PathVariable("id") Integer id) {
+        return "nacos registry, serverPort: " + serverPort + "\t id" + id;
+    }
+}
