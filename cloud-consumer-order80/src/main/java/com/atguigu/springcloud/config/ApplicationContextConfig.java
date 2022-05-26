c70267c4d8197fa4424f9d7d89e14de62bd9f3c9
wxj01 <wangxinjian_01@163.com>
2022-05-26 15:05:57 +0800

    代码调整

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/config/ApplicationContextConfig.java
@@ -1,20 +1,20 @@
-//package com.atguigu.springcloud.config;
-//
-//import org.springframework.beans.factory.annotation.Configurable;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.client.RestTemplate;
-//
-///**
-// * @author Administrator
-// * @version 1.0
-// * @description: TODO
-// * @date 2021/4/11 0011 16:09
-// */
-//@Configurable
-//public class ApplicationContextConfig {
-//
-//    @Bean
-//    public RestTemplate getRestTemplate(){
-//        return new RestTemplate();
-//    }
-//}
+package com.atguigu.springcloud.config;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/4/11 0011 16:09
+ */
+@Configurable
+public class ApplicationContextConfig {
+
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
